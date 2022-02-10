b15e97f418b88a882884e11390e90f1dc594bc1b
Vitor Melo <viitor.melo@hotmail.com>
2022-02-10 18:32:16 -0300

    conversão de peso e calculo de desconto com IF

---
+++ main.py
@@ -0,0 +1,17 @@
+'''peso = float(input("Qual o seu peso? "))
+sistema = input("(K)g ou (L)bs? ")
+if sistema == "K" or sistema =="k":
+    resultado = peso*2.205
+    print("Peso em libras=" +str(resultado))
+if sistema == "L" or sistema =="l":
+    resultado = peso/2.205
+    print("Peso em Kg= " +str(resultado))'''
+
+'''p=float(input('Qual o preço do produto? R$'))
+pergunta=input('[1] Calcular desconto\n[2] Saber desconto aplicado')
+if pergunta == '1':
+    d=float(input('Quanto de desconto aplicar? '))
+    print('O valor com {} de desconto é de {}'.format(d,(p*(100-d)/100)))
+if pergunta == '2':
+    d=float(input('Quanto o produto está custando? R$'))
+    print('O produto está com {}% de desconto'.format((p-(p*(d/100)))))'''
